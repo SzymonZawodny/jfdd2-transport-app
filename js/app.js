e59209d9764a98ed3e628d9c12c197b73c48d12d
Mateusz Rybczonek <m.rybczonek@gmail.com>
2016-04-01 19:29:37 +0200

    - hover transformations added to buttons;

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 angular.module('transportApp', ['ngAnimate', 'ui.bootstrap']);
 
-
+//hamburger menu
 angular.module('transportApp')
   .controller('collapseMenu', function ($scope) {
   $scope.isCollapsed = true;
 });
+
+
