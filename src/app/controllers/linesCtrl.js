312dadcedf784a2525c8e117f4d712819f81d7d5
mateuszRybczonek <m.rybczonek@gmail.com>
2016-04-15 16:51:52 +0200

    - added tables for tab2;

--- src/app/controllers/linesCtrl.js
+++ src/app/controllers/linesCtrl.js
@@ -1,4 +1,11 @@
 function linesCtrl($scope, lineDetailsService) {
-
   $scope.linesDetails = lineDetailsService.getLinesDetails();
+  $scope.readLineStops = readLineStops;
+
+  function readLineStops(busLine){
+    $scope.selectedLineObject = $scope.linesDetails
+      .filter(function(singleBusLine){
+        return singleBusLine.line === busLine;
+      });
+  }
 }
